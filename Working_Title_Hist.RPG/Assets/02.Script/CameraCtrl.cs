a2552a3d31c83ee25a6c1889a7a30518e9b93561
BaekGunWoo1119 <qorrjsdn668@sungkyul.ac.kr>
2023-06-24 21:43:19 +0900

    2023-06-24-백건우
    회의록 정리 및 회의 전까지 작업된 파일 업로드

---
+++ Working_Title_Hist.RPG/Assets/02.Script/CameraCtrl.cs
@@ -0,0 +1,23 @@
+//카메라가 플레이어를 따라오게 만드는 스크립트
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+
+    float offsetY = 10.0f;
+    float offsetZ = -5.0f;
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =new Vector3(player.transform.position.x, player.transform.position.y +  offsetY, player.transform.position.z + offsetZ);
+    }
+}
