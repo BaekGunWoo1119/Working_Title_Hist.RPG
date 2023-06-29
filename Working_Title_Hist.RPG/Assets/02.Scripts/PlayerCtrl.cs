d83202dac38ad96cbe0940e99aa846e892d1b6f1
BaekGunWoo1119 <qorrjsdn668@sungkyul.ac.kr>
2023-06-29 12:22:39 +0900

    2023-06-29 백건우
    맵 해결, 몬스터 프리펩 생성 반영, 그 외 여러 설정들 변경 적용 후 업로드

---
+++ Working_Title_Hist.RPG/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float moveSpeed = 50.0f;
+    public float rotSpeed = 5.0f;
+    private void Move_Pos(Vector3 move) {this.transform.position += move;}
+    private Vector3 Get_Pos() {return this.transform.position;}
+      
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move_Pos(Vector3.left*moveSpeed*Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            Move_Pos(Vector3.right*moveSpeed*Time.deltaTime);
+        }  
+        if(Input.GetKey(KeyCode.W))
+        {
+            Move_Pos(Vector3.forward*moveSpeed*Time.deltaTime);
+        }  
+        if(Input.GetKey(KeyCode.S))
+        {
+            Move_Pos(Vector3.back*moveSpeed*Time.deltaTime);
+        }
+
+        transform.Rotate(0f, Input.GetAxis("Mouse X") * rotSpeed, 0f, Space.World);
+
+    }
+}
